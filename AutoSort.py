d43c9962687b672b24e3fbaaa120e3069362a44c
PMaestroM <pierre.mignot49@gmail.com>
2019-03-31 13:59:28 +0200

    Manage empty string in the creation date

--- AutoSort.py
+++ AutoSort.py
@@ -9,13 +9,11 @@ fileList = os.listdir(src)
 for fileName in fileList:
     date = time.ctime(os.path.getmtime(src+fileName))
     fileNameArr = date.split(" ")
+    for checkName in fileNameArr:
+        if checkName == '':
+            fileNameArr.remove(checkName)
     dirsName = fileNameArr[0]+"_"+fileNameArr[2]+"_"+fileNameArr[1]+"_"+fileNameArr[4]
     os.makedirs(dest+dirsName, exist_ok=True)
     os.rename(src+fileName, dest+dirsName+"/"+fileName)
 
-
-
-
-
-
-
+print("Sort done.")
